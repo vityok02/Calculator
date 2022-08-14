53d6c4d4fa03860651c5020206e1bf88b35c2d04
Віктор Липкань <lipkan02@gmail.com>
2022-08-14 10:40:10 +0300

    Add project files.

---
+++ Calculator/Operations.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculator;
+
+class Calculate
+{
+    public void DoOperation()
+    {
+        string operation = GetOperation();
+        switch (operation)
+        {
+            case "+":
+                Console.WriteLine(operation);
+                break;
+        }
+    }
+    public static string GetValueFromConsole()
+    {
+        string operation = Console.ReadLine();
+        return operation;
+    }
+
+    public static string GetOperation()
+    {
+        string operation = Console.ReadLine();
+        return operation;
+    }
+}
